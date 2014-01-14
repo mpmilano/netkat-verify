10e970cc935964c60d473cac046ace8c71180d81
Matthew Milano <milano@cs.cornell.edu>
2014-01-14 18:56:26 -0500

    convenience script for running command-line reachability from file

---
+++ run-reachability-from-file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+file="$1"
+verify="`find . -name Verify.byte -print -quit`"
+if [[ $verify ]] 
+then 
+    echo -n ''
+else
+    verify="`which Verify.byte`"
+fi
+
+name="`egrep '^Name:' $file | sed s/'^Name:'//g`"
+input="`egrep '^Input:' $file | sed s/'^Input:'//g`"
+program="`egrep -v -e '^Input:' -v -e '^Name:' -v -e '^Output:' -v -e '^Result:' $file | sed s/'^Program:'//`"
+output="`egrep '^Output:' $file | sed s/'^Output:'//g`"
+result="`egrep '^Result:' $file | sed s/'^Result:'//g`"
+
+echo -n "starting verify. input: "
+echo -n "$input"
+echo -n ", output: "
+echo "$output"
+$verify -f NetKAT -m reach "$name" "$input" "$program" "$output"
+
